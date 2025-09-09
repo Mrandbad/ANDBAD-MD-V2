130f5f4b605c6f2f2042c4057a2d4c9619d34ac2
ANDBAD-MD-V1 <mallyaandrea361@gmail.com>
2025-09-09 20:06:53 +0300

    Update main-updater.js

--- plugins/main-updater.js
+++ plugins/main-updater.js
@@ -16,24 +16,24 @@ cmd({
     if (!isOwner) return reply("This command is only for the bot owner.");
 
     try {
-        await reply("🔍 Checking for CRISS-AI updates...");
+        await reply("🔍 Checking for 𝚫𝚴𝐃𝚩𝚫𝐃-𝚫𝚰updates...");
 
         // Fetch the latest commit hash from GitHub
-        const { data: commitData } = await axios.get("https://api.github.com/repos/criss-vevo/CRISS-AI/commits/main");
+        const { data: commitData } = await axios.get("https://github.com/Mrandbad/ANDBAD-MD-V2/commits/main");
         const latestCommitHash = commitData.sha;
 
         // Get the stored commit hash from the database
         const currentHash = await getCommitHash();
 
         if (latestCommitHash === currentHash) {
-            return reply("✅ Your CRISS-AI bot is already up-to-date!");
+            return reply("✅ Your 𝚫𝚴𝐃𝚩𝚫𝐃-𝚫𝚰 bot is already up-to-date!");
         }
 
-        await reply("🚀 Updating CRISS-AI Bot...");
+        await reply("🚀 Updating 𝚫𝚴𝐃𝚩𝚫𝐃-𝚫𝚰 Bot...");
 
         // Download the latest code
         const zipPath = path.join(__dirname, "latest.zip");
-        const { data: zipData } = await axios.get("https://github.com/criss-vevo/CRISS-AI/archive/main.zip", { responseType: "arraybuffer" });
+        const { data: zipData } = await axios.get("https://github.com/Mrandbad/ANDBAD-MD-V2/archive/main.zip", { responseType: "arraybuffer" });
         fs.writeFileSync(zipPath, zipData);
 
         // Extract ZIP file
@@ -44,7 +44,7 @@ cmd({
 
         // Copy updated files, preserving config.js and app.json
         await reply("🔄 Replacing files...");
-        const sourcePath = path.join(extractPath, "CRISS-AI-main");
+        const sourcePath = path.join(extractPath, "𝚫𝚴𝐃𝚩𝚫𝐃-𝚫𝚰-main");
         const destinationPath = path.join(__dirname, '..');
         copyFolderSync(sourcePath, destinationPath);
 
