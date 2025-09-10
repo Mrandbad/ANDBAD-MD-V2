87c5f90d4bd992c8655a192b33a0d9893f651cd7
ANDBAD-MD-V1 <mallyaandrea361@gmail.com>
2025-09-10 23:43:27 +0300

    Update main-repo.js

--- plugins/main-repo.js
+++ plugins/main-repo.js
@@ -38,7 +38,7 @@ async (conn, mek, m, { from, reply }) => {
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363417599637828@newsletter',
+                    newsletterJid: '120363315949714553@newsletter',
                     newsletterName: '𝚫𝚴𝐃𝚩𝚫𝐃 𝚫𝚰',
                     serverMessageId: 143
                 }
@@ -55,7 +55,7 @@ async (conn, mek, m, { from, reply }) => {
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363417599637828@newsletter',
+                    newsletterJid: '120363315949714553@newsletter',
                     newsletterName: '𝚫𝚴𝐃𝚩𝚫𝐃 𝚫𝚰',
                     serverMessageId: 143
                 }
