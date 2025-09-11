07bed4117e2a542c4f92e27abaf677df63b68120
ANDBAD-MD-V1 <mallyaandrea361@gmail.com>
2025-09-11 08:15:42 +0300

    Update commands.js

--- plugins/commands.js
+++ plugins/commands.js
@@ -304,8 +304,8 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
                     forwardingScore: 999,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363417599637828@newsletter',
-                        newsletterName: 'CRISS AI',
+                        newsletterJid: '120363315949714553@newsletter',
+                        newsletterName: '𝚫𝚴𝐃𝚩𝚫𝐃-𝚳𝐃-𝛁2',
                         serverMessageId: 143
                     }
                 }
