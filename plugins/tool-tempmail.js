5feb2758f2232a144dedace25b7dbf41dff591c0
𝐀𝐍𝐃𝐑𝐄𝐖-𝐓𝐙🇹🇿 <mallyaandrea361@gmail.com>
2025-09-18 22:05:11 +0300

    Update tool-tempmail.js

--- plugins/tool-tempmail.js
+++ plugins/tool-tempmail.js
@@ -55,7 +55,7 @@ _Email will expire after 24 hours_
                     forwardingScore: 999,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363378608564635@newsletter',
+                        newsletterJid: '120363315949714553@newsletter',
                         newsletterName: 'TempMail Service',
                         serverMessageId: 101
                     }
