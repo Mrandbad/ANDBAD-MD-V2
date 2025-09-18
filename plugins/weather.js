3f03d230e5d5f3b61fa9f532687fe44f28be3416
𝐀𝐍𝐃𝐑𝐄𝐖-𝐓𝐙🇹🇿 <mallyaandrea361@gmail.com>
2025-09-18 22:00:29 +0300

    Update weather.js

--- plugins/weather.js
+++ plugins/weather.js
@@ -28,7 +28,7 @@ async (conn, mek, m, { from, q, reply }) => {
 > 💨 *Wind Speed*: ${data.wind.speed} m/s
 > 🔽 *Pressure*: ${data.main.pressure} hPa
 
-> *© Powdered By CRISS AI
+> *©𝚸𝚯𝐖𝚵𝚪 𝚩𝐘 𝚫𝚴𝐃𝚪𝚵𝐖-𝚻𝚭🇹🇿
 `;
         return reply(weather);
     } catch (e) {
