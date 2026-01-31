b0ca3f7e51989e2c805b0eff1ac1f084c4ffd482
Fredi AI <frediezra60@gmail.com>
2026-01-31 13:09:04 +0300

    Add files via upload

---
+++ utility/botUtil/Ownermiddleware.js
@@ -0,0 +1,18 @@
+const Ownermiddleware = async (context, next) => {
+    const { m, Owner } = context;
+
+    if (!Owner) {
+        return m.reply(`┏━━━━━━━━━━━━━━━━━━━━➤
+┃✿ You dare attempt to use Owner command? 
+
+┃✿ Your mere existence is an insult to my code. 
+
+┃✿ Crawl back into the abyss where mediocrity thrives before I personally wipe you from this reality fool.😌
+━━━━━━━━━━━━━━━━━━━━
+ `);
+    }
+
+    await next();
+};
+
+module.exports = Ownermiddleware;
---
+++ utility/botUtil/middleware.js
@@ -0,0 +1,24 @@
+const middleware = async (context, next) => {
+    const { m, isBotAdmin, isAdmin } = context;
+
+    if (!m.isGroup) {
+        return m.reply(`┏━━━━━━━━━━━━━━━━━➤
+┃✿ This command isn’t for lone wolves. Try again in a group. 🐺
+━━━━━━━━━━━━━━━━━`);
+    }
+    if (!isAdmin) {
+        return m.reply(`┏━━━━━━━━━━━━━━━━━➤
+┃✿ You think you’re worthy? 
+┃✿ Admin privileges are required—go beg for them. 😤
+━━━━━━━━━━━━━━━━━`);
+    }
+    if (!isBotAdmin) {
+        return m.reply(`┏━━━━━━━━━━━━━━━━━➤
+┃✿ I need admin rights to obey, unlike you who blindly follows. 🫵 
+━━━━━━━━━━━━━━━━━`);
+    }
+
+    await next(); // Proceed to the next function (main handler)
+};
+
+module.exports = middleware;
