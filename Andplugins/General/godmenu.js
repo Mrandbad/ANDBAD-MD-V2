4652df3d59297fb93294c2b7506b195002b06396
DML <daudymussa1705@gmail.com>
2026-02-13 04:36:41 -0800

    Update godmenu.js

--- Andplugins/General/godmenu.js
+++ Andplugins/General/godmenu.js
@@ -33,7 +33,7 @@ module.exports = {
 
     menuText += `╭─➤ 「 LOGO 🎨 」\n`;
 
-    let commandFiles = fs.readdirSync('./andplugins/God').filter(file => file.endsWith('.js'));
+    let commandFiles = fs.readdirSync('./Andplugins/God').filter(file => file.endsWith('.js'));
     for (const file of commandFiles) {
       const commandName = file.replace('.js', '');
       const fancyCommandName = toFancyFont(commandName);
